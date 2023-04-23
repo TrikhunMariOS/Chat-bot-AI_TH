7b5d32680d0e49fe107cb858a3fd7317474df14d
TrikhunMariOS <131513803+TrikhunMariOS@users.noreply.github.com>
2023-04-23 15:25:23 +0700

    Chatbot
    ตัวโปรแกรมรัน Chatbot และโปรแกรมเทรนนิ่ง AI

--- chatbot.py
+++ chatbot.py
@@ -2,13 +2,11 @@ import random
 import json
 import pickle
 import numpy as np
+import datetime
 
-import nltk
-from nltk.stem import WordNetLemmatizer
-
+from pythainlp.tokenize import word_tokenize
 from tensorflow.keras.models import load_model
 
-lemmatizer = WordNetLemmatizer()
 with open('D:\\pythonProject6\\Scripts\\intents.json', encoding='utf-8') as f:
     intents = json.load(f)
 
@@ -18,8 +16,7 @@ model = load_model('D:\\pythonProject6\\Scripts\\chatbot_model.model.h5')
 
 
 def clean_up_sentence(sentence):
-    sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    sentence_words = word_tokenize(sentence, keep_whitespace=False)
     return sentence_words
 
 
@@ -27,11 +24,12 @@ def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
     bag = [0] * len(words)
     for w in sentence_words:
-        for i,word in enumerate(words):
+        for i, word in enumerate(words):
             if word == w:
                 bag[i] = 1
     return np.array(bag)
 
+#ชุดข้อมูลตอบคำถามทั่วไป
 def predict_class(sentence):
     bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
@@ -44,15 +42,57 @@ def predict_class(sentence):
         return_list.append({'intents':classes[r[0]], 'probability':str(r[1])})
     return return_list
 
-def get_response(intents_list, intents_json):
-    tag = intents_list[0]['intents']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if i['tag'] == tag:
-            result = random.choice(i['responses'])
-            return result
+#ชุดข้อมูลตอบคำถามพิเศษ"นำข้อมูลจากภายนอกมาใช้"
+def get_current_time(): #เวลา
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime("%H:%M:%S")
+    return f"ตอนนี้เป็นเวลา {formatted_time} ค่ะ"
 
+def get_current_date(): #วัน/เดือน/ปี
+    current_date = datetime.datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d")
+    return f"วันนี้คือวันที่ {formatted_date} ค่ะ"
+
+def get_current_day(): #วันอะไร
+    current_day = datetime.datetime.now()
+    day_of_week = current_day.strftime("%A")
+    days_translation = {
+        'Monday': 'วันจันทร์',
+        'Tuesday': 'วันอังคาร',
+        'Wednesday': 'วันพุธ',
+        'Thursday': 'วันพฤหัสบดี',
+        'Friday': 'วันศุกร์',
+        'Saturday': 'วันเสาร์',
+        'Sunday': 'วันอาทิตย์'
+    }
+    day_in_thai = days_translation[day_of_week]
+    return f"วันนี้คือ {day_in_thai} ค่ะ"
+
+#------------------------------------------------------------------------------------------------
+
+#การตอบสนองต่อคำถาม
+def get_response(intents_list, intents_json):
+    if intents_list:
+        tag = intents_list[0]['intents']
+        list_of_intents = intents_json['intents']
+        for i in list_of_intents:
+            if i['tag'] == tag:
+                if tag == "ask_time":
+                    result = get_current_time()
+                elif tag == "ask_date":
+                    result = get_current_date()
+                elif tag == "ask_day":
+                    result = get_current_day()
+                else:
+                    result = random.choice(i['responses'])
+                return result
+    else:
+        return "ขอโทษค่ะ ฉันไม่เข้าใจคำถามของคุณ กรุณาลองใหม่อีกครั้ง"
+
+
+print("-----------------")
 print("Start bot system")
+print("-----------------")
 
 while True:
     message = input("")
--- training.py
+++ training.py
@@ -3,34 +3,30 @@ import json
 import pickle
 import numpy as np
 
-import nltk
-nltk.download('wordnet')
-from nltk.stem import WordNetLemmatizer
+from pythainlp.tokenize import word_tokenize
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 
-lemmatizer = WordNetLemmatizer()
-
+#นำเข้าข้อมูลไฟล์จาก intents.json
 with open('intents.json', encoding='utf-8') as f:
     intents = json.load(f)
-
 
 words = []
 classes = []
 documents = []
-ignore_letters = ['?','!','.',',']
+ignore_letters = ['?','!','.',',','น่ะ','เอิ่ม','พอดี','แล้ว','อ่ะ','อะ',' ']
 
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        word_list = nltk.word_tokenize(pattern)
+        word_list = word_tokenize(pattern, keep_whitespace=False)
         words.extend(word_list)
-        documents.append((word_list,intent['tag']))
+        documents.append((word_list, intent['tag']))
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = [word for word in words if word not in ignore_letters]
 words = sorted(set(words))
 
 classes = sorted(set(classes))
@@ -38,14 +34,12 @@ classes = sorted(set(classes))
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
-
 training = []
 output_empty = [0] * len(classes)
 
 for document in documents:
     bag = []
     word_patterns = document[0]
-    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
     for word in words:
         bag.append(1) if word in word_patterns else bag.append(0)
 
@@ -66,7 +60,7 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-
+#การเทรนนิ่ง AI-----------------------------------------------------
 from tensorflow.keras.optimizers.schedules import ExponentialDecay
 
 initial_learning_rate = 0.01
@@ -78,6 +72,11 @@ sgd = SGD(learning_rate=lr_schedule, momentum=0.9, nesterov=True)
 
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+#Early stopping เป็นเทคนิคที่ใช้ในการฝึกอบรมโมเดล deep learning เพื่อป้องกัน overfitting ,โดยจะหยุดการเทรนนิ่งเมื่อโมเดลไม่ไม่การปรับปรุงเพิ่มเติม
+from tensorflow.keras.callbacks import EarlyStopping
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=500, batch_size=5, verbose=1, validation_split=0.2, callbacks=[early_stopping])
 model.save('chatbot_model.model.h5',hist)
-print('done')
+print('----------------','AI Training is done','----------------')
